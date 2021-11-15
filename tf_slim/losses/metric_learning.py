e00575ad39d19112a4b1342930825258316cf233
Pulkit Sinha <pulkitsinha@google.com>
2021-11-15 09:56:09 -0800

    Internal Google change
    PiperOrigin-RevId: 410009606
    Change-Id: Ie70858ad073cfc02a3481d5891f49fbe17a14b2d

--- tf_slim/losses/metric_learning.py
+++ tf_slim/losses/metric_learning.py
@@ -16,7 +16,6 @@
 """Implements various metric learning losses."""
 from __future__ import absolute_import
 from __future__ import division
-
 from __future__ import print_function
 
 import importlib
